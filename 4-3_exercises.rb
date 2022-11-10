f6d6807beb07ac8904051764bcd2715088ad9635
Yuluki <web.yulukitaiga@gmail.com>
2022-11-10 15:57:12 +0900

    演習）4-3-1

---
+++ 4-3_exercises.rb
@@ -0,0 +1,7 @@
+puts "① ================================"
+nums = { one: "uno", two: "dos", three: "tres"}
+nums.each do |key, value|
+  puts "'#{key}'はスペイン語で'#{value}'"
+end
+puts "② ================================"
+
