aaa26d5367ed847f360083b5eae1e91b8cf28d8b
Yuluki <web.yulukitaiga@gmail.com>
2022-11-10 16:33:04 +0900

    modify

--- 4-3_exercises.rb
+++ 4-3_exercises.rb
@@ -1,8 +1,11 @@
+# 1：ハッシュの文字列
 puts "① ================================"
 nums = { one: "uno", two: "dos", three: "tres"}
 nums.each do |key, value|
   puts "'#{key}'はスペイン語で'#{value}'"
 end
+
+# 2：ハッシュのハッシュを確認
 puts "② ================================"
 person1 = { first: "Taro", last: "Yamada"}
 person2 = { first: "Emi", last: "Yamada"}
@@ -15,12 +18,15 @@ puts "person1 is father!" if params[:father][:first] == person1[:first]
 puts "person2 is mother!" if params[:mother][:first] == person2[:first]
 puts "person3 is child!" if params[:child][:first] == person3[:first]
 
+# 3：メソッドチェーンを使用する
 puts "③ ================================"
 user = {}
 user[:name] = "hogeo"
 user[:email] = "example@mail"
 user[:password_digest] = ('a'..'z').to_a.shuffle[0..15].join
 p user
+
+# 4：Ruby APIを使う。Hashクラスのmergeメソッド
 puts "④ ================================"
 merge_hash = { "a" => 100, "b" => 200 }.merge({ "b" => 300 })
 p merge_hash
