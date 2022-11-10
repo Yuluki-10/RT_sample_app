4c6bbabee979bebdf140effacfc9f61544623b29
Yuluki <web.yulukitaiga@gmail.com>
2022-11-10 16:08:50 +0900

    演習）4-3-2

--- 4-3_exercises.rb
+++ 4-3_exercises.rb
@@ -4,4 +4,13 @@ nums.each do |key, value|
   puts "'#{key}'はスペイン語で'#{value}'"
 end
 puts "② ================================"
-
+person1 = { first: "Taro", last: "Yamada"}
+person2 = { first: "Emi", last: "Yamada"}
+person3 = { first: "Jiro", last: "Yamada"}
+params = {}
+params[:father] = person1
+params[:mother] = person2
+params[:child] = person3
+puts "person1 is father!" if params[:father][:first] == person1[:first]
+puts "person2 is mother!" if params[:mother][:first] == person2[:first]
+puts "person3 is child!" if params[:child][:first] == person3[:first]
